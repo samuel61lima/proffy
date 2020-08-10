60cf09c54751d599d1a7c0d4b2696e0ea56786de
Samuel Lima <samuel61lima@gmail.com>
2020-08-10 00:44:16 -0300

    construindo back-end

---
+++ web/server/src/routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import ClassesController from "./controllers/ClassesController";
+
+const routes = express.Router();
+const ClassesControllers = new ClassesController();
+
+routes.get("/classes", ClassesControllers.index);
+routes.post("/classes", ClassesControllers.create);
+
+export default routes;
